20766466d10e953a06b0b06ca3db84ef01256c33
dparalen <vetrisko@gmail.com>
2013-07-25 16:07:47 +0200

    fix: check whether an element appears by issuing its .is_displayed() method, too

--- pageobjects/events.py
+++ pageobjects/events.py
@@ -1,14 +1,14 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium_wrapper import SE
 
-def appears(locator, timeout=0.5):
+def appears(locator, timeout=5.0, message=""):
     '''a decorator to wait till the locator is able to locate the element
     i.e. till element appears. Assumes locator doesn't need the driver
     instance passed as attribute. Locators are supposed to use
     selenium_wrapper.SE instead.
     '''
     def waitnlocate(*args, **kvargs):
-        WebDriverWait(SE, timeout=timeout).until(lambda x: locator(*args, **kvargs))
+        WebDriverWait(SE, timeout=timeout).until(lambda x: locator(*args, **kvargs).is_displayed(), message=message)
         return locator(*args, **kvargs)
 
     return waitnlocate
