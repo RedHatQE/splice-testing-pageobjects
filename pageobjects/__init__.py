d24f085a9dfdfafe0e4dd4483f5cc70f29bf9416
dparalen <vetrisko@gmail.com>
2013-07-29 16:12:17 +0200

    refactoring and new filter menu introduced

--- pageobjects/__init__.py
+++ pageobjects/__init__.py
@@ -8,13 +8,16 @@ locators = {
     "login.logout": lambda : SE.find_element_by_link_text("logout"),
     "login.logout_notice": lambda: SE.find_element_by_xpath("//li[text()='You have been logged out']"),
 
+    "filters.filter.base.close_link": lambda : SE.find_element_by_xpath("//a[@class='close' and @href='#' and @data-close='panel' and text() = 'Close']"),
     "filters.page_title": lambda: SE.find_element_by_xpath("//header/h2[@original-title='Report Filters']"),
     "filters.filter": lambda filter_name: SE.find_element_by_xpath("//div/div/span[normalize-space(text()) = '%s']/../.." % filter_name),
-    "filters.filter.close_link": lambda : SE.find_element_by_xpath("//a[@class='close' and @href='#' and @data-close='panel' and text() = 'Close']"),
     "filters.filter.selected": lambda filter_name: SE.find_element_by_xpath("//div[@id='filter_name' and @title='%s']" % filter_name),
     "filters.filter.run_button": lambda : SE.find_element_by_xpath("//a[contains(@href, 'reports')]/div[@id='submit_button' and normalize-space(text())='Run Report']/.."),
     "filters.filter.encrypt_export": lambda : SE.find_element_by_xpath("//input[@id='splice_reports_export_encrypt_cbox' and @type='checkbox']"),
-    "filters.filter.skip_json_export": lambda : SE.find_element_by_xpath("//input[@id='splice_reports_include_json_cbox' and @type='checkbox']") 
+    "filters.filter.skip_json_export": lambda : SE.find_element_by_xpath("//input[@id='splice_reports_include_json_cbox' and @type='checkbox']"),
+    "filters.new": lambda : SE.find_element_by_xpath("//a[@id='new' and @data-ajax_url='/sam/splice_reports/filters/new']"),
+    "filters.new.selected": lambda : SE.find_element_by_xpath("//h2[@title='New Filter']"),
+    "filters.new.name": lambda : SE.find_element_by_xpath("//input[@id='splice_reports_filter_name']"),
 
 }
 
