edc7899fb944e8c379cddd22caf440ff06a85526
dparalen <vetrisko@gmail.com>
2013-08-22 15:19:16 +0200

    adopting updated basepageobject features

--- pageobjects/login.py
+++ pageobjects/login.py
@@ -3,42 +3,32 @@ from  . import locators, pages
 from basepageelement import InputPageElement, ButtonPageElement, LinkPageElement
 from basepageobject import BasePageObject
 from selenium.common.exceptions import NoSuchElementException
+from nose.tools import assert_in, assert_raises
 
 from contextlib import contextmanager
 
 class LoginPageObject(BasePageObject):
+    _sub_url = pages.login.url
     username = InputPageElement(locators.login.username)
     password = InputPageElement(locators.login.password)
     submit_button = ButtonPageElement(locators.login.submit)
 
-    def __init__(self):
-        try:
-            # are we already there?
-            self.assertEqual(pages.login.title, SE.title)
-        except AssertionError as e:
-            SE.get(SE.current_url + pages.login.url)
-            self.assertEqual(pages.login.title, SE.title)
-
+    
     def submit(self):
         # assumes successful log-in by the Sign "Out link" presence
         self.submit_button.click()
         SE.refresh()
-        self.assertIn(locators.login.logout, SE)
+        assert_in(locators.login.logout, SE)
 
 class LogoutPageObject(BasePageObject):
+    _sub_url = pages.logout.url
     logout_link = LinkPageElement(locators.login.logout)
 
-    def __init__(self):
-        try:
-            self.assertEqual(pages.login.title, SE.title)
-        except AssertionError as e:
-            SE.get(SE.current_url + pages.login.url)
-            self.assertEqual(pages.login.title, SE.title)
-
+ 
     def submit(self):
         self.logout_link.click()
         # assert the logout link isn't there anymore
-        with self.assertRaises(NoSuchElementException):
+        with assert_raises(NoSuchElementException):
             self.logout_link
 
 def login(username, password):
