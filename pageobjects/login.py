092627806f888890b174eced56999a09ac46736c
dparalen <vetrisko@gmail.com>
2013-08-30 11:39:00 +0200

    introducing signo-login--logout independence

--- pageobjects/login.py
+++ pageobjects/login.py
@@ -1,35 +1,50 @@
 from selenium_wrapper import SE, restore_url
-from  . import locators, pages
+from  . import locators, pages, events
 from basepageelement import InputPageElement, ButtonPageElement, LinkPageElement
-from basepageobject import BasePageObject
-from selenium.common.exceptions import NoSuchElementException
+from basepageobject import RedirectingPageObject
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from nose.tools import assert_in, assert_raises
 
 from contextlib import contextmanager
 
-class LoginPageObject(BasePageObject):
+class LoginPageObject(RedirectingPageObject):
     _sub_url = pages.login.url
+
     username = InputPageElement(locators.login.username)
     password = InputPageElement(locators.login.password)
+    default_org_link = LinkPageElement(events.appears(locators.login.default_org_link))
     submit_button = ButtonPageElement(locators.login.submit)
 
-    
-    def submit(self):
-        # assumes successful log-in by the Sign "Out link" presence
-        self.submit_button.click()
-        SE.refresh()
-        assert_in(locators.login.logout, SE)
+    def _assertUrl(self):
+        # overriding to handle signo--non-signo redirects
+        if not pages.login.check_url in SE.current_url and not pages.login.signo.url in SE.current_url:
+            raise AssertionError("page %s not loaded" % self.__class__.__name__)    
 
-class LogoutPageObject(BasePageObject):
+    def submit(self):
+        if pages.login.signo.url in SE.current_url:
+            # using signo
+            self.submit_button.click()
+        else:
+            self.submit_button.click()
+            # non-signo: selecting the default org required
+            # it also kinda asserts log-in succeeded
+            self.default_org_link.click()
+        # assumes successful log-in by the dashboard page navigation
+        assert_in(pages.dashboard.url, SE.current_url)
+
+class LogoutPageObject(RedirectingPageObject):
     _sub_url = pages.logout.url
-    logout_link = LinkPageElement(locators.login.logout)
 
- 
+    def _assertUrl(self):
+        # overriding to handle signo--non-signo redirects
+        try:
+            events.appears(locators.login.submit, timeout=1) in SE
+        except TimeoutException:
+            raise AssertionError("page %s not loaded" % self.__class__.__name__)
+       
     def submit(self):
-        self.logout_link.click()
-        # assert the logout link isn't there anymore
-        with assert_raises(NoSuchElementException):
-            self.logout_link
+        # calling is preceeded by _navigate-ing self; just pass
+        pass
 
 def login(username, password):
     '''Performs a log in'''
